0ba13cd72b82d2583965bae531d20bba3a6145ac
Mudasir Ali <mudasiralinizamani@gmail.com>
2021-09-27 14:54:05 +0500

    Added User and Auth routes

--- routers/authRouter.go
+++ routers/authRouter.go
@@ -1,1 +1,11 @@
 package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mudasiralinizamani/ums-api/controllers"
+)
+
+func AuthRouter(app *gin.Engine) {
+	app.POST("/auth/signup", controllers.Signup())
+	app.POST("/auth/signin", controllers.Signin())
+}
--- routers/userRouter.go
+++ routers/userRouter.go
@@ -3,8 +3,11 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/mudasiralinizamani/ums-api/controllers"
+	"github.com/mudasiralinizamani/ums-api/middlewares"
 )
 
 func UserRouter(app *gin.Engine) {
+	app.Use(middlewares.Authenticate())
 	app.GET("/users", controllers.GetUsers())
+	app.GET("/users/:user_id", controllers.GetUser())
 }
